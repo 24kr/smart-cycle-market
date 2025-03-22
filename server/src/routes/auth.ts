d5c995eef743d98145098548c8e429058d81d194
NGABO MBARAGA Eric <ericngabombaraga24@gmail.com>
2025-03-22 20:06:28 +0200

    Removed Error

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,20 +1,20 @@
 import { Router } from "express";
-import { createNewUser, grantValid, generateVerificationLink,
-     grantAccessToken, sendProfile, signIn, signOut, verifyEmail } from "controllers/auth";
+import { createNewUser, generateForgetPassLink, generateVerificationLink,
+     grantAccessToken, grantValid, sendProfile, signIn, signOut, verifyEmail } from "controllers/auth";
 import validate from "src/middleware/validator";
 import { newUserSchema, verifyTokenSchema } from "src/utils/validationSchema";
-import { isValidPassResetToken } from './../middleware/isAuth';
+import { isAuth, isValidPassResetToken } from './../middleware/isAuth';
 
 const authRouter=Router();
 
 authRouter.post('/sign-up', validate(newUserSchema), createNewUser);
 authRouter.post('/verify', validate(verifyTokenSchema),verifyEmail);
-authRouter.get('/verify-token', isValidPassResetToken, generateVerificationLink);
+authRouter.get('/verify-token', isAuth, generateVerificationLink);
 authRouter.post('/sign-in', signIn);
-authRouter.get('/profile', isValidPassResetToken, sendProfile);
+authRouter.get('/profile', isAuth, sendProfile);
 authRouter.post('/refresh-token', grantAccessToken);
-authRouter.post('/sign-out', isValidPassResetToken, signOut);
-authRouter.post('/forget-pass', grantValid);
+authRouter.post('/sign-out', isAuth, signOut);
+authRouter.post('/forget-pass', generateForgetPassLink);
 authRouter.post('/verify-pass-reset-token', validate(verifyTokenSchema), isValidPassResetToken, grantValid);
 
 export default authRouter;      
